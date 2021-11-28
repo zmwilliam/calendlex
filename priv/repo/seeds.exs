b6de87a05403ee3d0ad885112a8501a62ed162df
zmwilliam <willcxa@gmail.com>
2021-11-28 20:02:40 -0300

    Adding EventType seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,37 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Calendlex.EventType
+alias Calendlex.Repo
+
+Repo.delete_all(EventType)
+
+[
+  %{
+    name: "15 minute meeting",
+    description: "Short meeting call.",
+    slug: "15-minute-meeting",
+    duration: 15,
+    color: "blue"
+  },
+  %{
+    name: "30 minute meeting",
+    description: "Extended meeting call.",
+    slug: "30-minute-meeting",
+    duration: 30,
+    color: "pink"
+  },
+  %{
+    name: "Pair programming session",
+    description: "One hour of pure pair programming fun!",
+    slug: "pair-programming-session",
+    duration: 60,
+    color: "purple"
+  }
+]
+|> Enum.each(fn event_type ->
+  event_type
+  |> EventType.changeset()
+  |> Repo.insert!()
+end)
